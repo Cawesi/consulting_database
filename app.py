5f626daf845a8e1d1e9f28fdfec43a3ecf81ca32
Wellington Siqueira <147565201+Cawesi@users.noreply.github.com>
2025-05-11 03:29:28 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import json
+
+app = Flask( __name__ )
+
+@app.route( '/' )
+def flikker():
+    json_file = './usuarios.json'
+
+    with open( json_file, encoding='utf-8' ) as file:
+        json_file = json.load( file )
+
+    return json_file
+
+if __name__ == '__main__':
+    app.run()
